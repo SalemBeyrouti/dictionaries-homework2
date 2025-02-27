70e8cb75fc3a05344464c1308952d504dd042049
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-27 22:21:04 +0200

    finished defining the dictionary and added all infos

--- exercise3.py
+++ exercise3.py
@@ -8,4 +8,14 @@ for i in range (departments):
 
     company_employees[department_name] = {}
 
+    num_employees = int(input("Enter the number of employees in department: "))
+
+    for j in range(num_employees):
+        employee_name = input("Enter the name of the employee: ")
+        employee_age = int(input("Enter the age of the employee: "))
+        employee_role = input("Enter role of employee: ")
+
+        company_employees[department_name][employee_age] = {"age": employee_age, "role": employee_role}
+
+
 print (company_employees)
