fe54e551ec57eb4a9ed1d4c6607a571ccc2cbdfe
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-27 23:29:54 +0200

    finalised the problem before turning in

--- exercise3.py
+++ exercise3.py
@@ -1,57 +1,59 @@
-# company_employees = {}
+company_employees = {}
 
-# #for the 2nd part i have to ensure that "Engineering" is always a department
+#for the 2nd part i have to ensure that "Engineering" is always a department
 
-# company_employees["Engineering"] = {}
+company_employees["Engineering"] = {}
 
 
-# departments = int(input("Enter number of departments: "))
+departments = int(input("Enter number of departments: "))
 
-# for i in range (departments):
+for i in range (departments):
 
-#     department_name = input("Enter the name of the department: ")
-#     if department_name not in company_employees:
+    department_name = input("Enter the name of the department: ")
+    if department_name not in company_employees:
     
-#      company_employees[department_name] = {}
+     company_employees[department_name] = {}
 
-#     num_employees = int(input(f"Enter the number of employees in {department_name} department: "))
+    num_employees = int(input(f"Enter the number of employees in {department_name} department: "))
 
-#     for j in range(num_employees):
-#         employee_name = input("Enter the name of the employee: ")
-#         employee_age = int(input(f"Enter the age of {employee_name}: "))
-#         employee_role = input(f"Enter role of {employee_name}: ")
-# # thought it was more clear to impliment f inside input 
-#         company_employees[department_name][employee_name] = {"age": employee_age, "role": employee_role}
+    for j in range(num_employees):
+        employee_name = input("Enter the name of the employee: ")
+        employee_age = int(input(f"Enter the age of {employee_name}: "))
+        employee_role = input(f"Enter role of {employee_name}: ")
+# thought it was more clear to impliment f inside input 
+        company_employees[department_name][employee_name] = {"age": employee_age, "role": employee_role}
 
-
-# # print (company_employees)
-
-# # #2nd part of the exercise
-
-# company_employees["Engineering"]["David"] = {"age": 27, "role": "Data Scientist"}
 
 # print (company_employees)
 
-# def count_total_employees(company): 
-#     #when defining a fucntion the parameter isnide is just a placeholder
-#     #i can name it whatver i want it can not be defined
-#     #it loopss through each key in the defined dictionary
-#    total_count = 0
-#    for department in company.values():
-#         #.values used to access all the values in the dictionary
-#         total_count += len(department)
+# #2nd part of the exercise
 
-#    return total_count
+company_employees["Engineering"]["David"] = {"age": 27, "role": "Data Scientist"}
+
+print (company_employees)
+
+def count_total_employees(company): 
+    #when defining a fucntion the parameter isnide is just a placeholder
+    #i can name it whatver i want it can not be defined
+    #it loopss through each key in the defined dictionary
+   total_count = 0
+   for department in company.values():
+        #.values used to access all the values in the dictionary
+        total_count += len(department)
+
+   return total_count
    
-# total_employees = count_total_employees(company_employees)
-# print(f"Total number of employees in the company: {total_employees}")
+total_employees = count_total_employees(company_employees)
+print(f"Total number of employees in the company: {total_employees}")
 
+
+#last part of the problem
 action = "yes"
 dict_input = {}
 
-while action != "no":
-   name = input("Enter an emplotyee name: (type no if u wanna stop)")
-   if name == "no":
+while action.lower() != "no": #takes into consideration uppercases, saw it while researching
+   name = input("Enter an employee name: (type no if u wanna stop)")
+   if name.lower() == "no":  #same as before for uppercase 
       break
    number = int(input("Enter how many of the same name exist: "))
    dict_input[name] = number
