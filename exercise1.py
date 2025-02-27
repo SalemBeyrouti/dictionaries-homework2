1596bc17b6624408fa8785259b1c20a8c0b3460a
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-27 02:39:49 +0200

    finalised the exercise with the merge function

--- exercise1.py
+++ exercise1.py
@@ -11,19 +11,25 @@ while True:
 
     dict1[key1]=value
 
-print (dict1)
+# print (dict1)
 
-# dict2 = {}
+dict2 = {}
 
-# print("Enter a value-key pair for the 2nd dictionary: ")
+print("Enter a value-key pair for the 2nd dictionary: ")
 
-# while True:
-#     key2 = input("Enter a key for the 2nd dictionary (type done when you wanna stop): ")
+while True:
+    key2 = input("Enter a key for the 2nd dictionary (type done when you wanna stop): ")
 
-#     if key2 == 'done':
-#         break
-#     value = int(input(f"Enter a value for '{key2}': "))
+    if key2 == 'done':
+        break
+    value = int(input(f"Enter a value for '{key2}': "))
 
-#     dict2[key2]=value
+    dict2[key2]=value
 
 # print (dict2)
+
+mergeddictionaries = {**dict1, **dict2}
+
+#after several searches i thought this was the optimal formula and the easiest to mnemorise
+
+print ("The merged dictionary is: ", mergeddictionaries)
