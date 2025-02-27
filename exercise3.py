68af62be21ad5ee3f9e400c187b90da48b7aa89f
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-27 22:39:01 +0200

    solved the b part for new employee

--- exercise3.py
+++ exercise3.py
@@ -1,12 +1,18 @@
 company_employees = {}
 
+#for the 2nd part i have to ensure that "Engineering" is always a department
+
+company_employees["Engineering"] = {}
+
+
 departments = int(input("Enter number of departments: "))
 
 for i in range (departments):
 
     department_name = input("Enter the name of the department: ")
-
-    company_employees[department_name] = {}
+    if department_name not in company_employees:
+    
+     company_employees[department_name] = {}
 
     num_employees = int(input(f"Enter the number of employees in {department_name} department: "))
 
@@ -15,7 +21,13 @@ for i in range (departments):
         employee_age = int(input(f"Enter the age of {employee_name}: "))
         employee_role = input(f"Enter role of {employee_name}: ")
 # thought it was more clear to impliment f inside input 
-        company_employees[department_name][employee_age] = {"age": employee_age, "role": employee_role}
+        company_employees[department_name][employee_name] = {"age": employee_age, "role": employee_role}
 
+
+# print (company_employees)
+
+#2nd part of the exercise
+
+company_employees["Engineering"]["David"] = {"age": 27, "role": "Data Scientist"}
 
 print (company_employees)
