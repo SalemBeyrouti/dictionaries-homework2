e834ee51bdae0d21dbe4b5ae521fc6eff837225a
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-27 02:14:25 +0200

    added the 1st dictionary in the 1st exercise

---
+++ exercise1.py
@@ -0,0 +1,11 @@
+dict1 = {}
+
+print("Enter a key-value pair in the 1st dictionary: ")
+
+while "true":
+    key1 = input("Enter a key for the 1st dictionary: ")
+
+    if key1 == 'done':
+        break
+    value = int(input(f"Enter a value for '{key1}: "))
+
