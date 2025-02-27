e7c0034c30ea999e2c2f2176c219ae83bfeb198d
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-02-27 22:24:04 +0200

    implemented f inside each input

--- exercise3.py
+++ exercise3.py
@@ -8,13 +8,13 @@ for i in range (departments):
 
     company_employees[department_name] = {}
 
-    num_employees = int(input("Enter the number of employees in department: "))
+    num_employees = int(input(f"Enter the number of employees in {department_name} department: "))
 
     for j in range(num_employees):
         employee_name = input("Enter the name of the employee: ")
-        employee_age = int(input("Enter the age of the employee: "))
-        employee_role = input("Enter role of employee: ")
-
+        employee_age = int(input(f"Enter the age of {employee_name}: "))
+        employee_role = input(f"Enter role of {employee_name}: ")
+# thought it was more clear to impliment f inside input 
         company_employees[department_name][employee_age] = {"age": employee_age, "role": employee_role}
 
 
